73277785ee62e24e37a8b46ed1ff56ac1e3d7c57
André Luiz <contato@xdvl.info>
2015-12-06 21:10:17 -0200

    transforma Parceiro e Palestra em atributos Inline de Evento; Permite clonar eventos

--- pyrio/eventos/admin.py
+++ pyrio/eventos/admin.py
@@ -3,6 +3,14 @@ from django.contrib import admin
 from pyrio.eventos import models
 
 
+class ParceiroInline(admin.TabularInline):
+    model = models.Parceiro
+
+
+class PalestraInline(admin.TabularInline):
+    model = models.Palestra
+
+
 @admin.register(models.Evento)
 class EventoAdmin(admin.ModelAdmin):
     list_display = [
@@ -28,31 +36,24 @@ class EventoAdmin(admin.ModelAdmin):
         }),
     ]
 
+    actions = [
+        'copiar_evento',
+    ]
+
+    inlines = [
+        PalestraInline,
+        ParceiroInline,
+    ]
+
+    def copiar_evento(self, request, queryset):
+        for instance in queryset:
+            instance.pk = None
+            instance.nome += ' (Cópia)'
+            instance.save()
+
 
 @admin.register(models.Palestrante)
 class PalestranteAdmin(admin.ModelAdmin):
     list_display = [
         'nome',
-    ]
-
-
-@admin.register(models.Palestra)
-class PalestraAdmin(admin.ModelAdmin):
-    list_display = [
-        'titulo',
-    ]
-
-    list_filter = [
-        'evento',
-    ]
-
-
-@admin.register(models.Parceiro)
-class ParceiroAdmin(admin.ModelAdmin):
-    list_display = [
-        'nome',
-    ]
-
-    list_filter = [
-        'evento',
     ]
